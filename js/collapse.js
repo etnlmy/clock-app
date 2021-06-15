32d66deee034f126a4b904f073b2e8e441bdab94
Etienne Lamy <etienne.lamydlc@gmail.com>
2021-06-15 19:39:43 +0700

    implements mobile version + collapse detail panel

---
+++ js/collapse.js
@@ -0,0 +1,26 @@
+function collapse({
+  button,
+  main,
+  aside
+}) {
+
+  const LESS = "less";
+  const MORE = "more";
+
+  const span = button.querySelector("span");
+  const asideHeight = aside.clientHeight;
+  let collapsed = false;
+
+  button.addEventListener("click", () => {
+    collapsed = !collapsed;
+    span.innerText = collapsed ? LESS : MORE;
+    main.style.marginTop = collapsed ? `-${asideHeight}px` : "0";
+    collapsed ? button.classList.add(LESS) : button.classList.remove(LESS);
+  });
+}
+
+collapse({
+  button: document.getElementById("collapse"),
+  main: document.getElementsByTagName("main")[0],
+  aside: document.getElementsByTagName("aside")[0],
+});
