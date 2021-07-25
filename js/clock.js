0174e64eb58f6e7399f99dfd9b1c5304cc5428c6
Etienne Lamy <etienne.lamydlc@gmail.com>
2021-07-25 14:21:56 +0700

    configure redirect for non https API

--- js/clock.js
+++ js/clock.js
@@ -32,7 +32,7 @@ function clock({
   clockContainer
 }) {
 
-  const TIME_API_URL = "https://worldtimeapi.org/api/ip";
+  const TIME_API_URL = "/worldtimeapi/ip";
   const LOCATION_API_URL = "https://freegeoip.app/json/";
 
   function getGreeting(hours) {
